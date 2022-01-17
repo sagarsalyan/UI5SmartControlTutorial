0af80637f6a61a38821ca61f483b3a88f2cec5b1
Sagar Salyan <sagarslyn@gmail.com>
2022-01-17 07:49:09 +0000

    added smart link

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,34 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"app/SmartControlTutorial/shellMock/UShellCrossApplicationNavigationMock"
+], function (Controller,UShellCrossApplicationNavigationMock) {
 	"use strict";
 
 	return Controller.extend("app.SmartControlTutorial.controller.App", {
 		onInit: function () {
 			this.getView().bindElement("/Products('4711')");
+			
+			// for smart link
+			UShellCrossApplicationNavigationMock.mockUShellServices({
+				tutorial_03_Name: {
+					links: [
+						{
+							action: "sap_se",
+							intent: "http://www.sap.com",
+							text: "SAP SE"
+						}, {
+							action: "sap_sapphire",
+							intent: "http://www.sap.com/sapphire",
+							text: "SAP Sapphire"
+						}, {
+							action: "app3",
+							intent: "http://www.sap.com/hana",
+							text: "App3"
+						}
+					]
+				}
+			});
+			// for smart link
 		}
 	});
 });
